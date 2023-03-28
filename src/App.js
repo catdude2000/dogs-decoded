7ad83a6d3881bd054e686bf9380e3d606b902d4c
Michael Pace <michael.g.pace@gmail.com>
2023-03-28 21:22:39 +0000

    added axios import, added to state, added handleinput,added catch to getdogs, struggling with env, added proper functions to form

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+// require('dotenv').config();
+
+// let APP_SERVER = process.env.REACT_APP_SERVER;
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstDogsPulled: []
-
+      firstDogsPulled: [],
+      dogShown: [],
+      showDog: false
     };
   }
+
+handleInput = (event) => {
+  this.setState({
+    dogEntered: event.target.value
+  });
+}
 
   getDogs = async (event) => {
     event.preventDefault();
     try {
-      let dogResults = await axios.get(`${process.env.APP_SERVER}`)
+      // console.log(APP_SERVER, 'APPSERVER')
+      let dogResults = await axios.get(`${process.env.APP_SERVER}/dogInfo`);
+      console.log(dogResults, 'dogResults');
+      this.setState({
+        dogShown: dogResults.data,
+        showDog: true
+      })
     }
-  }
+    catch (error) {
+      this.setState({
+        error: true,
+        errorMessage: `An error ocurred: ${error.response.status}`
+      })
+    }
+  };
   
 
 
     render() {
       return (
         <>
-          <form id='form' onSubmit={this.submitDogHandler}>
+          <form id='form' onSubmit={this.getDogs}>
             <label>
               Have a breed in mind?
-              <input type='text' />
+              <input type='text' onInput={this.handleInput}/>
             </label>
             <button type='submit'>Search</button>
           </form>
