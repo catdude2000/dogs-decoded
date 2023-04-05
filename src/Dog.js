4e36d9a347d23272c2c9eea5c69e50ca4815e8e9
Michael Pace <michael.g.pace@gmail.com>
2023-04-05 03:40:06 +0000

    added rest of stats to dog js

--- src/Dog.js
+++ src/Dog.js
@@ -12,16 +12,43 @@ class Dog extends React.Component {
                     <Modal.Title>Name = {this.props.dogFeatured.name}
                     </Modal.Title>
                 {/* </Modal.Header> */}
-                <Modal.Body>Typical barking amount = {this.props.dogFeatured.barkingAmount}
+                <Modal.Body>
+                    {this.props.dogFeatured.image}
                     <br/>
-                Trainability = {this.props.dogFeatured.trainable}
+                    Good With Kids: {this.props.dogFeatured.goodWithKids}
                     <br/>
-                Energy Level = {this.props.dogFeatured.energyAmount} 
+                    Good With Dogs: {this.props.dogFeatured.goodWithDogs}
                     <br/>
-                Protectiveness = {this.props.dogFeatured.howProtective} 
+                    Amount of Shedding: {this.props.dogFeatured.shedAmount}
                     <br/>
-                Life Expectancy = {this.props.dogFeatured.minLifeExpectancy}
-                -{this.props.dogFeatured.maxLifeExpectancy} years
+                    Grooming Effort Required: {this.props.dogFeatured.groomingNeeded}
+                    <br/>
+                    Drool Factor: {this.props.dogFeatured.drool}
+                    <br/>
+                    Coat Length: {this.props.dogFeatured.furLength}
+                    <br/>
+                    Good With Strangers: {this.props.dogFeatured.goodWithOtherPeople}
+                    <br/>
+                    Playfulness: {this.props.dogFeatured.playful}
+                    <br/>
+                    Typical barking amount = {this.props.dogFeatured.barkingAmount}
+                    <br/>
+                    Trainability = {this.props.dogFeatured.trainable}
+                    <br/>
+                    Energy Level = {this.props.dogFeatured.energyAmount} 
+                    <br/>
+                    Protectiveness = {this.props.dogFeatured.howProtective} 
+                    <br/>
+                    Life Expectancy = {this.props.dogFeatured.minLifeExpectancy}
+                    -{this.props.dogFeatured.maxLifeExpectancy} years
+                    <br/>
+                    Male Height = {this.props.dogFeatured.minHeightMale} - {this.props.dogFeatured.maxHeightMale}
+                    <br/>
+                    Male Weight = {this.props.dogFeatured.minWeightMale} - {this.props.dogFeatured.maxWeightMale}
+                    <br/>
+                    Female Height = {this.props.dogFeatured.minHeightFemale} - {this.props.dogFeatured.maxHeightFemale}
+                    <br/>
+                    Female Weight = {this.props.dogFeatured.minWeightFemale} - {this.props.dogFeatured.maxWeightFemale}
                 </Modal.Body>
             </Modal>
             {/* <Card show={this.props.dogAppears}>dadadasd 
